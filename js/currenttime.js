6a2b39e142d4e7a3edf6139e53947fc507ca19e6
melissahalldesign <hal19005@byui.edu>
2019-09-21 14:25:09 -0600

    time

--- js/currenttime.js
+++ js/currenttime.js
@@ -1,7 +1,13 @@
 function GetTime() {
-    var modiDate = new Date(document.GetTime);
     var modiDate = new Date();
-    var CurTime = modiDate.getHours() + ":" + modiDate.getMinutes();
+    
+
+    var modiDate = new Date();
+    var CurTime = modiDate.getHours() + ":" + modiDate.getMinutes()
     return CurTime
 }
-document.write(GetTime();
+
+
+document.write(GetTime());
+
+document.write("");
