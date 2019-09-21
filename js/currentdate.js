eb2a016a5ffcea8b8115fa6367ca662b2160e47a
melissahalldesign <hal19005@byui.edu>
2019-09-21 11:00:44 -0600

    adding time js

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,3 +1,3 @@
-const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', time: 'numeric'};
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
   
