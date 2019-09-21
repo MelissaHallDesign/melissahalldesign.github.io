b475f2b877edde44b452fcd3f0df8a963f5254f7
melissahalldesign <hal19005@byui.edu>
2019-09-21 12:57:54 -0600

    date

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,3 +1,4 @@
 const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', time: 'numeric'};
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+new Date(new Date().setDate(new Date().getDate() + 1))
   
