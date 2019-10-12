7dab050d2d77da02b0172e4c1172c02c3c6e7242
melissahalldesign <hal19005@byui.edu>
2019-10-12 17:25:29 -0600

    fixing js

--- Lesson4/js/currentDay.js
+++
@@ -1,3 +0,0 @@
-const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById('currentday').textContent = new Date().toLocaleDateString('en-GB', options);
-  
