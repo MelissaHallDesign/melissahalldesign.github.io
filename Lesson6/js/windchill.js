18eed737f83c733ae3054d3ffc14fdd365349d42
melissahalldesign <hal19005@byui.edu>
2019-10-26 12:32:41 -0600

    fixed js

--- Lesson6/js/windchill.js
+++ Lesson6/js/windchill.js
@@ -1,7 +1,7 @@
 //f = 35.74 + 0.6215t - 35.75s(0.16) + 0.4275ts(0.16)
 //chill = 35.74 + 0.6215(temp) - (35.75(speed)0.16) + (0.4275) * temp * speed * 0.16
-var temp = parseFloat(document.getElementById('temp').textContent);
-var speed = parseFloat(document.getElementById('speed').textContent);
+var temp = parseFloat(document.getElementById('temp')[0].textContent);
+var speed = parseFloat(document.getElementById('speed')[0].textContent);
 
 if (temp <= 50 && speed >= 3) {
     var calc = 35.74 + temp * 0.6215 - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
