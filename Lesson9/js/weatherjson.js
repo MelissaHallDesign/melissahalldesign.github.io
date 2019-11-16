4697dc16becb3178b8c8130bf9d102a8f4176427
melissahalldesign <hal19005@byui.edu>
2019-11-15 21:58:20 -0700

    updated js again

--- Lesson9/js/weatherjson.js
+++ Lesson9/js/weatherjson.js
@@ -4,34 +4,34 @@ fetch(requestURL)
         return response.json();
     })
     .then(function (jsonObject) {
-        const towns = jsonObject['towns'];
         console.table(jsonObject); //temporary testing
+        const towns = jsonObject['towns'];
+        const idahoTowns = ["Fish Haven", "Soda Springs", "Preston"];
         for (let i = 0; i < towns.length; i++) { 
-            if (towns[i] ==="Fish Haven" || towns[i] ==="Soda Springs" || towns[i] ==="Preston"){
+            if (idahoTowns.includes(towns[i].name)) {
             let card = document.createElement('section');
-            let span = document.createElement('span');
             let h2 = document.createElement('h2');
-            let motto = document.createElement('i');
+            let motto = document.createElement('p');
             let yearFounded = document.createElement('p');
             let currentPopulation = document.createElement('p');
             let averageRainfall = document.createElement('p');
             let image = document.createElement('img');
+            card.className = 'towncards';
+            motto.className = 'motto';
             h2.textContent = towns[i].name;
             motto.textContent = towns[i].motto;
             yearFounded.textContent =  'Year Founded: ' + towns[i].yearFounded;
             currentPopulation.textContent =  'Population: ' + towns[i].currentPopulation;
             averageRainfall.textContent =  'Annual Rain Fall: ' + towns[i].averageRainfall;
-            card.appendChild(span);
-            span.appendChild(h2);
-            span.appendChild(motto);
-            span.appendChild(yearFounded);
-            span.appendChild(currentPopulation);
-            span.appendChild(averageRainfall);
-            card.appendChild(image);
             image.setAttribute('src', 'images/' + towns[i].photo);
             image.setAttribute('alt', towns[i].name);
-            
-            document.getElementsByClassName('towncards').appendChild(card);
+            card.appendChild(h2);
+            card.appendChild(motto);
+            card.appendChild(yearFounded);
+            card.appendChild(currentPopulation);
+            card.appendChild(averageRainfall);
+            card.appendChild(image);
+            document.getElementsByClassName('div.towncards').appendChild(card);
         }
         }
     });
