fb3bed7b8506e72a5311f05418cad333a1a799df
melissahalldesign <hal19005@byui.edu>
2019-11-16 15:31:52 -0700

    .

--- Lesson9/js/weatherjson.js
+++ Lesson9/js/weatherjson.js
@@ -28,12 +28,13 @@ fetch(requestURL)
             image.setAttribute('src', 'images/' + towns[i].photo);
             image.setAttribute('alt', towns[i].name);
             //Merge them together
-            card.appendChild(div);
+            
             span.appendChild(h2);
             span.appendChild(motto);
             span.appendChild(yearFounded);
             span.appendChild(currentPopulation);
             span.appendChild(averageRainfall);
+            card.appendChild(span);
             card.appendChild(image);
             //Attaching to the page
             document.querySelector('div.towncards').appendChild(card);
