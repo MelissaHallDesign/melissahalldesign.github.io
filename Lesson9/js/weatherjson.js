7ced06be3c67607a0feec70b10a77580589222e9
melissahalldesign <hal19005@byui.edu>
2019-11-16 17:03:41 -0700

    updated css

--- Lesson9/js/weatherjson.js
+++ Lesson9/js/weatherjson.js
@@ -13,7 +13,7 @@ fetch(requestURL)
             let card = document.createElement('section');
             let span = document.createElement('span');
             let h3 = document.createElement('h3');
-            let motto = document.createElement('i');
+            let motto = document.createElement('p');
             let yearFounded = document.createElement('p');
             let currentPopulation = document.createElement('p');
             let averageRainfall = document.createElement('p');
