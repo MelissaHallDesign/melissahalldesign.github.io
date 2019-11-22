6f3a5e7dd7fd57e09ab6b648d008bffe7f89282f
melissahalldesign <hal19005@byui.edu>
2019-11-21 21:49:37 -0700

    updated js

--- Lesson10/js/prestonweatherapi.js
+++ Lesson10/js/prestonweatherapi.js
@@ -14,10 +14,28 @@ fetch(apiURL)
     document.getElementById('speed')
     .textContent = jsObject.wind.speed;
 
-    runWindChill();
+    const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=8edd374362f96360d97da10d2909e8b7";
+    fetch(apiURL)
+      .then((response) => response.json())
+      .then((jsObject) => {
+        console.log(jsObject);
+        for (let i = 0; i < jsObject.list.length; i++){
+          if (jsObject.list[i].dt_txt.includes('18:00:00')) {
+            console.log(jsObject.list[i].main.temp.toFixed(0));
+          }
+        }
+      });
+
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+    const desc = jsObject.weather[0].description;  // note how we reference the weather array
+    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    document.getElementById('icon').setAttribute('alt', desc);
+
+    //runWindChill();
   });
   function runwindChill(){
-    var temp = parseFloat(document.getElementById('temp').textContent);
+    var temp = parseFloat(document.getElementById('current-temp').textContent);
     var speed = parseFloat(document.getElementById('speed').textContent);
   if (temp <= 50 && speed >= 3) {
     var calc = 35.74 + temp * 0.6215 - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
@@ -27,8 +45,8 @@ fetch(apiURL)
     document.getElementById('calc').innerHTML = 'N/A';
 }
 }
-function calc(temp, speed) {
-  var result = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 0.16);
+/*function calc(temp, speed) {
+  var result = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
         return result;
 }
   /*var temp = parseFloat(document.getElementById('temp').textContent);
