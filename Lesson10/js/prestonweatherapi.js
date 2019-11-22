cd59583c4cd73afbe60189bd63b344eb73a60f20
melissahalldesign <hal19005@byui.edu>
2019-11-21 21:11:40 -0700

    .

--- Lesson10/js/prestonweatherapi.js
+++ Lesson10/js/prestonweatherapi.js
@@ -13,8 +13,24 @@ fetch(apiURL)
     .textContent = jsObject.main.humidity;
     document.getElementById('speed')
     .textContent = jsObject.wind.speed;
-  });
 
+    runWindChill();
+  });
+  function runwindChill(){
+    var temp = parseFloat(document.getElementById('temp').textContent);
+    var speed = parseFloat(document.getElementById('speed').textContent);
+  if (temp <= 50 && speed >= 3) {
+    var calc = 35.74 + temp * 0.6215 - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+    document.getElementById('calc').innerHTML = Math.round(calc);
+}
+  else {
+    document.getElementById('calc').innerHTML = 'N/A';
+}
+}
+function calc(temp, speed) {
+  var result = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 0.16);
+        return result;
+}
   /*var temp = parseFloat(document.getElementById('temp').textContent);
 var speed = parseFloat(document.getElementById('speed').textContent);
 if (temp <= 50 && speed >= 3) {
