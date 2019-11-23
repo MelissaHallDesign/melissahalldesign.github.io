9b5338346378788ab373f95ec28172743606a727
melissahalldesign <hal19005@byui.edu>
2019-11-22 21:09:00 -0700

    .

--- Lesson10/js/cityforecastapi.js
+++ Lesson10/js/cityforecastapi.js
@@ -1,7 +1,7 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473,5607916,5585010&units=imperial&APPID=8edd374362f96360d97da10d2909e8b7";
+const apiURL2 = "https://api.openweathermap.org/data/2.5/forecast?id=5604473,5607916,5585010&units=imperial&APPID=8edd374362f96360d97da10d2909e8b7";
 
 
-fetch(apiURL)
+fetch(apiURL2)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
@@ -15,8 +15,8 @@ fetch(apiURL)
     const imagesrc = 'https://openweathermap.org/img/wn/' + jsObject.list[i].weather[0].icon + '.png';  // note the concatenation
     const desc = jsObject.weather[0].description;  // note how we reference the weather array
     document.getElementById('pimg_' + counter).setAttribute('src', imagesrc);  // focus on the setAttribute() method
-    document.getElementById('pimg' + counter).setAttribute('alt', desc);
-    counter ++;
+    document.getElementById('pimg_' + counter).setAttribute('alt', desc);
+    counter++;
       }
     }
   });
