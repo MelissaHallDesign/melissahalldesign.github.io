ceaf50c6e7ef1e45ae98ea051892e5a93340283e
melissahalldesign <hal19005@byui.edu>
2019-11-22 20:44:51 -0700

    .

--- Lesson10/js/cityforecastapi.js
+++ Lesson10/js/cityforecastapi.js
@@ -5,13 +5,13 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    document.getElementById('temp')
-    .textContent = jsObject.main.temp;
-    document.getElementById('icon')
-    .textContent = jsObject.main.temp;
-    document.getElementById('current-temp')
-    .textContent = jsObject.main.temp;
 
+    for (let i = 0; i < jsObject.list.length; i++){
+      if (jsObject.list[i].dt_txt.includes('18:00:00')) {
+        console.log(jsObject.list[i].main.temp.toFixed(0));
+    document.getElementById('temp')
+    .textContent = jsObject.list[i].main.temp;
+    
     const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
     const desc = jsObject.weather[0].description;  // note how we reference the weather array
     document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
