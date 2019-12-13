1342f1309226da030a6fac43ba1beb173bbcb92d
melissahalldesign <hal19005@byui.edu>
2019-12-12 22:08:57 -0700

    updated json and javascript, css for json file. Fixed url for json.

--- final-project/js/temples.js
+++ final-project/js/temples.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://melissahalldesign.github.io/final-project/json/temple.json';
+const requestURL = 'https://api.myjson.com/bins/nd9rc';
 fetch(requestURL) 
     .then(function (response) {
         return response.json();
@@ -12,18 +12,55 @@ fetch(requestURL)
             let templename = document.createElement('p');
             let templeaddress = document.createElement('p');
             let phone = document.createElement('p');
+            let email = document.createElement('p');
+            let services = document.createElement('p');
+            let history = document.createElement('p');
+            let bapschedule = document.createElement('p');
+            let inischedule = document.createElement('p');
+            let sealschedule = document.createElement('p');
+            let closure = document.createElement('p');
             let image = document.createElement('img');
-            h2.textContent = temples[i].templename + ' ' + temples[i].templename;
-            templeaddress.textContent = 'Date of Birth: ' + temples[i].templeaddress;
-            phone.textContent =  'Place of Birth: ' + temples[i].phone;
+            h2.textContent = temples[i].templename;
+            templeaddress.textContent = temples[i].templeaddress;
+            phone.textContent = temples[i].phone;
+            email.textContent = temples[i].email;
+            services.textContent = 'Services available: ' + temples[i].services;
+            history.textContent = 'History: ' + temples[i].history;
+            bapschedule.textContent = 'Baptism schedule: ' + temples[i].bapschedule;
+            inischedule.textContent = 'Initiatory schedule: ' + temples[i].inischedule;
+            sealschedule.textContent = 'Sealings schedule: ' + temples[i].sealschedule;
+            closure.textContent = 'Closure schedule: ' + temples[i].closure;
             image.textContent = temples[i].templename + ' ' + temples[i].imageurl;
             image.setAttribute('src', temples[i].imageurl);
-            image.setAttribute('alt', temples[i].templename + ' ' + temples[i].templename + ' - ' + temples[i].order);
+            image.setAttribute('alt', temples[i].templename);
             templecard.appendChild(h2);
             templecard.appendChild(templename);
+            templecard.appendChild(image);
             templecard.appendChild(templeaddress);
             templecard.appendChild(phone);
-            templecard.appendChild(image);
+            templecard.appendChild(email);
+            templecard.appendChild(services);
+            templecard.appendChild(history);
+            templecard.appendChild(bapschedule);
+            templecard.appendChild(inischedule);
+            templecard.appendChild(sealschedule);
+            templecard.appendChild(closure);
             document.querySelector('div.templecards').appendChild(templecard);
         }
     });
+
+    /*
+            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+            birthdate.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+            birthplace.textContent =  'Place of Birth: ' + prophets[i].birthplace;
+            image.textContent = prophets[i].name + ' ' + prophets[i].imageurl;
+            image.setAttribute('src', prophets[i].imageurl);
+            image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + prophets[i].order);
+            card.appendChild(h2);
+            card.appendChild(birthdate);
+            card.appendChild(birthplace);
+            card.appendChild(image);
+            document.querySelector('div.cards').appendChild(card);
+        }
+    });
+    */
